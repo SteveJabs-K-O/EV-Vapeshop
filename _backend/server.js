8361abe4a2d97f839877b1324988674ab60b7fe1
Steve <steve.arenas1@gmail.com>
2023-06-09 02:28:54 +0800

    installed cors and import to my server.js

--- _backend/server.js
+++ _backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import pool from "./config/db.js"; // just to log database status - for debugging purposes
 import apiRoutes from './router/apiRoute.js';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use('/', apiRoutes);
 
